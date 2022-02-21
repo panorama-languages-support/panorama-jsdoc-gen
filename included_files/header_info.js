d64d48c745c9fed970430918617ad94d67928ee5
braem <brae@momentum-mod.org>
2022-02-20 22:52:23 -0500

    chore: update header info
    shouldve been updated on last readme update

--- included_files/header_info.js
+++ included_files/header_info.js
@@ -1,5 +1,6 @@
 /**** Panorama JS Types ****/
 // This file is meant to provide JS typing information for Panorama Panels and APIs. It is by no means usable as actual code.
+// Having the file open in VS Code will automatically pull in the types all other open JS files.
 // If VS Code (or your editor of choice) does not automatically pick up the types, you can `require` it in JS files for development, but that will error in game.
 //
 // For typing information for specific panel types, you can use JSDoc on fetching panels via `$()`
